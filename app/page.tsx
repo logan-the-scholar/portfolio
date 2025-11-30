1f3f29037f758b6fec8d51804c109f0222a06e2c
Joshua-frDev <elvisteck046@outlook.com>
2025-11-29 23:36:26 -0500

    testing styles and wireframing stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,85 @@
 import Image from "next/image";
-
+// zinc-50
+// zinc-600
+// black
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="text-sm min-h-screen font-sans w-full">
+      <nav className="w-full border border-zinc-50 h-15 my-2 flex justify-between">
+        <div className="ml-4 h-14 border border-zinc-50 w-15">
+
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex flex-1 justify-around">
+          <div className="text-white items-center flex w-xl text-xl select-none font-console justify-around">
+            <span className="max-w-fit cursor-pointer h-fit">./projects</span>
+            <span className="max-w-fit cursor-pointer h-fit">./my story</span>
+            <span className="max-w-fit cursor-pointer h-fit">./about</span>
+          </div>
         </div>
-      </main>
+      </nav>
+      {/* <pre className="text-transparent bg-clip-text bg-linear-to-r from-black from-0% via-pink-500 via-33% via-yellow-500 via-66% to-black to-100% font-mono whitespace-pre text-xs overflow-hidden select-none"> */}
+      <pre className="text-transparent bg-clip-text hand-bg-gradient font-mono whitespace-pre text-xs overflow-hidden select-none">
+        {/* <code className="bg-linear-to-r from-black to-indigo-700 bg-clip-text"> */}
+        {`
+          +======-==-----------------:::-:-:-::-::::::::   :       -:::           :: :                              :-=        :::::: :                                          :::::::::::::::---::::::::-:::---
+          +====--==----------:::::::::::::-- ::::: :::-:  ::   :::  :::::               :                          :=           :::-::::                                            : :::: :::::::::::::::::::--==
+          =====-==========-----::::::::::-----:::::::::  :::: :-:  :  ::           ::      :                       :         :::                                                     :::::::::::::: :::::::::--+++
+          ====----=-===-----------:::::::-==--:::::::::    :::::     ::  :                                                     ::                                                      ::::::::::::::::::-::-=+===
+          ===----=-===-----=-----:: ::::::----:::::    : ::  ::                                                                :                                                       ::::::     ::::---:--=*===-
+          =------=--==---------:--::     :-:-::::  :      ::: :::::::    :                                     :                                            :                         : ::: ::::::::---==+**=--=--
+          ----------=----=---::::::       :-::----:::     : : :::: :                                          -               :                                                          :: : ::::::--===-------==
+          ----------::-----:::::::        :-::     :                                                        =       :                                                                             :=::::::-----:::
+          ::---::-:::::::::::::        ::::::::::                       :-:                                                                                                                      :=:   :::::::-:--
+          ---:::::::::::::::  ::         :::    :            :             -                             ::                                                                                      ::       :::::--:
+          ----::::::::::::::::                                            :                              =                                                                                       :     :::::::::::
+          -------::: ::: :::                                                                                                                                                                     :    :::::::::::-
+          -----:::::::::: ::::                   :                                                                                                                                               :     ::::::-::::
+          ------::-::       :                 :                         :                             :  :                                                                                       ::: :::::::::::::
+          -------:::   :::                   :                          ::                                       :                                                                              :::-:: :   :::::--
+          ----::::::::::           :       ::                           :::                                                                                                                    :   : : :::::::::::
+          ::-:::::::::::     :::   :::::::::              :===:         :                              :                                                                                            ::: ::::::::::
+          ::::::::::-:::         : ::::: : :         -=+=-----==*+**#%#+-:   :                                                                                                                      ::     :::::::
+          :::                   ::::: :::::: :    -==-::      :-++*#**++++*#*-                                                                                                                              ::::::
+          :-      : :: :         ::::: :::----: :----:::    :::--==+**+=---==+*%%**+*+--                                                                                                                    :  :::
+          ::    : :      :   ::       :        :-=+*######**++=--:::=++=-::------==**##***%                                                                                                           :   : :+####
+          ::::  :  :  :     :::::--:       ::-+*##%%@@@@@@@@@%+-::: :::---:::-----:::-++++%@:         :                                                                                               :-+=-:::-=-=
+          :-+*=:::::::  ::::::::       ::--=+**#%%%@@%%@@%@@@@@*-::     :---:-=-=-::::::--=#%@@#                                                                                               :+=-:::::::::::---=
+          ::::::::::::::::::::::::: ::::-==+**#%%@@%@@@@@@@@@@@@*=-:     :-=++*####+=::::::--*%@@@@:                                                          -                          :=::::::::::::::-----===+
+          -:::::::::::::::::::::::::::---==+##%@%@@@@@@@@@@@@@@@%*+=-:     ---+@@@%#*+%+---::=+-+*#%@@# :+:  :                                                         ::-=-::  ::::-----:::::   ::::::-------=+*#
+          -=-----------:::::::::::::::--=+*%%%@@@@@@@@@@@@@@-    #**+==:     :-+@@@@@@@@@%%@@*=-=---*+%@::                                          : :      :=-::::---::: :   :::::--:::-::::::::::::---====+*#%%
+          ===+=====------::----::----==+#%%@@@@@@@@@@@@@@=        +++#*=+:  :::-#@@%@@@@@@@@@@@@@@@*-:-#@::                                   : -=---::-=:----:::::-:--:::::----=------::::::::::-----==++*######%
+          +++++++====----------==++*##%%%@@@@@@@@@@@@@@:     :      =##%**=::::**#%@:-@@@@@@@@@@@@@@#=-=#@-               ::-::          ::-::: :-:: :::::-=:   :-+#%%#*****#**+===----::::::------========-----=-
+          *+**+++=====----=++#####%@@@@@@@@@@@@@@@@@@-     ::::::      -+*#%+==----*@@@@@@@@@@@@@@@@@@*+-#@-:  :   -=#+-::+=---==*+-:::---::::-=-: :::-::-++=:-=#%###@@@@@@%##*+==------------:::::::::::  ::::--+
+          +*++++++====+==+*###%%@@@@@@@@@@@@@@@@@@@=        :::               =++-:-=#@@@@@--@@@@@@@@# *+=@@   ++----##*#%@@*===+#* :- -+##**#%=:::::-:::=*%@@@@%*+#%@@@@@%#%#*++===---------::: :::::  :::::--=*#
+          ====-========+**#%%@@@@@@@@@@@@@@@@@@@@-          :                     *++++*%@@: :@@@@@@@@  +**%#:  +@@@@@@@@@@#=#@@@@@=-+=:=#@@%: :::--: ::=#@@@@@@@@@@%*+=++***+**+========------::::::::::::---=*##
+          -------==++***#%%%@@@@@@@@@@@@@@@@@@-                                    :*+*%@@#@   @@@@@@@    :::                      @@%@@*#@=  ::-*#-::-=*%@@@@@@@@@@@@@%%@@@@@@@@%@@###*#****++*++==+====--==+*###
+          --==-===++++**#%@@@@@@@@@@@@@@@@%                                         =@@@=--%+   @@@@@@                              :@@@#:  :-=*%@%++++#%@@@@@@@@@@@@@@@@@@@@%%@@%%%@@@@@@%%%%%#######*+***##%%%@@
+          ====++++++*##%%@@@@@@@@@@@@@@                                             #@@@@*-%    @@@@@@=                            *+*=   :-#%@@@@##%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%%%@@@@@@@@@@@@@@@@%@@%%%@@@@@@
+          ++++**###%%@@@@@@@@@@@@@@:                                                 #=          %% #@@                         ++==:=*--*%@@@@@@@@@@@@@@@@@@@@@@@: : -@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+          #%%%%%@@@@@@@@@@@@@@@@=                                                                                            :=:::=+=+#@@@@=-:-+@@%@@@@@@@@@@@@+         ::=%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+          @%%@@@@@@@@@@@@@@@@@:                                                                                               =##*:=#@@%==%@@=:-=%@=*#%@@@@@+           :::%+::-===--==+*==++*#%@@@@@@@@@@@@@@@@@@
+          @@@@@@@@@@@@@@@@#                                                                                                     +*#@@%==+%@@-#@@%*#@@@@@@+              ::=-:::::::::::::     ::::::::-+%@@@@@@@@#
+          @@@@@@@@@@@@#:                                                                              :                        -=+%@@+*@@@- =-+@@@+--+:              :    :::-:::: :  ::::     : :::::------+*+*+=
+          @@@@@@%#=:::                                                                                                         =*%@@%=*@@  -#@@@*::::::                ::::-  :    :             :::::::-=--===+++
+          :-::---::: ::                                                                                                        -*%@@+%@@-  +#@@@:                        : -:  :                   ::::::--===++++
+          -:-----::: :                                                                                                          +*@@+%@+   ++=%@                         :::    :             :    ::::-:====+++++
+          ::::::::::                                                                                                            +#@@*@@:   :: *:                           :                 :    ::-::--===-==+++
+          ::::-::                                                                                                               :=- -:                         : :         :   :      :   ::  ::::::::::--=--=====
+          :::::::    :                                                                                     ::                                 ::                       : : : :           :::::::::   :::--==---=--
+          :-::::::: ::                                                                                      :  :: :                                                   :::: ::            ::  :::::::    :-=-------
+          -----::::: :       :                                                                              :                                                        ::::-::::             :::--::-:::::---===-=--
+          =-=--=------::::::: :                                                                              :                                                   :::-:-:::::----:     : :::::-:-:-::-----=-====-=-
+          =--------:::=:::::::::                                                                            :  :                 :                               ::-::::::--::::::  ::::::::--------=---===+=-==-=
+          ===-=--::-------::::::                                                                           :   ::                                                :::::::::-::::::::::--::-----------------==--====
+          -==---::::-::::::::::::                                                                         :  :::                                              ::  ::  :::::::::::::------:--=---===------=-----===
+          -----:::::::-::::::::::                                                                     ::    :::::                 ::                       : :  ::    :::: :::: :-::-------=----=======+*+===--===
+          ------::::::--::::::::                                                                    -: :::::::::               :::                     : :      :   ::::::--:::--------------=-========*+=======++
+          ------:::::::--::::::::                                                                 :      :::::                ::::::    :               ::: :::::::::   :----------=--------====+====+============
+          -:--:::::-:----::::::::                                                                : : ::::    :       :      ::::::::: :             :::::::-:::-::::::-:::::::-:----------------===++=======+=====
+          --:-:-:-::---:::--::::::::::                                                          :      : :  :        :    : ::::::::::      ::   :   :::: ::::::::::::::: ::::-:-:-----:--------===+========++====
+          `}
+        {/* </code> */}
+      </pre>
     </div>
   );
 }
